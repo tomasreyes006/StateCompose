795dda356368bb829e6bbc755c2fafe4fac5ab8c
Tomás Reyes <treyes220@unab.edu.co>
2026-09-18 09:15:18 -0500

    Maquetación inicial

--- app/src/main/java/com/tomasreyes/statecompose/StateScreen.kt
+++ app/src/main/java/com/tomasreyes/statecompose/StateScreen.kt
@@ -1,2 +1,35 @@
 package com.tomasreyes.statecompose
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+
+@Preview(showSystemUi = true)
+@Composable
+fun StateScreen() {
+
+    Column(
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally,
+        modifier = Modifier.fillMaxSize()
+    ) {
+        Text(text = "Contador: 0", fontSize = 32.sp)
+        Spacer(modifier = Modifier.height(15.dp))
+        Button(onClick = {}) {
+
+            Text("Sumar")
+
+        }
+    }
+}
+
